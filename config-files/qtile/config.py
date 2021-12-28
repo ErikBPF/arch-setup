8a66e84a3d79f019127cf7d44283cc79611487c2
ErikBPF <erikbogado@gmail.com>
2021-12-28 01:35:30 -0300

    test

--- config-files/qtile/config.py
+++ config-files/qtile/config.py
@@ -543,3 +543,4 @@ focus_on_window_activation = "smart"
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
 wmname = "Qtile"
+#
