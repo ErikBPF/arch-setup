c9cce1540f1a1e4be43ff18ee253ac4865d6bf9f
ErikBPF <erikbogado@gmail.com>
2022-06-14 21:27:16 -0300

    change packages

--- config-files/qtile/autostart.sh
+++ config-files/qtile/autostart.sh
@@ -9,5 +9,5 @@ pulseeffects -w &
 dunst &
 feh --randomize --bg-fill ~/.config/wallpapers/wallpaper.png &
 betterlockscreen -u ~/.config/wallpapers &
-xidlehook --timer 180 "brightnessctl set 5%" "brightnessctl set 100%" &
-xidlehook --not-when-audio --timer 300 "betterlockscreen -l dim -off 10" &
+xidlehook --not-when-audio --timer 60 "brightnessctl set 5%" "brightnessctl set 100%" &
+xidlehook --not-when-audio --timer 75 "betterlockscreen -l dim --off 10" "" &
